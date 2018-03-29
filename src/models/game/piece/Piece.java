95e19ad26b4d1a687486a0179fd1ae61773e24ee
Jackson Ming Hu <huming2207@gmail.com>
2018-03-29 21:55:19 +1100

    Piece: add marking related stuff
    Signed-off-by: Jackson Ming Hu <huming2207@gmail.com>

--- src/models/game/piece/Piece.java
+++ src/models/game/piece/Piece.java
@@ -11,10 +11,24 @@ public interface Piece
     String getStyle();
 
     /**
-     * Set the value of attack level
+     * Set the value of privacy attack level
      *
      * @return Attack level value in x/100 marks
      */
     int getAttackLevel();
+
+    /**
+     * Apply attack from other pieces
+     *
+     * @param deduction HP value (mark) deduction
+     */
+    void applyAttack(int deduction);
+
+    /**
+     * Get current mark (max. 100?)
+     *
+     * @return current mark, 0 to 100
+     */
+    int getMark();
 
 }
