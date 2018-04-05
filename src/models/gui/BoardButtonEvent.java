99bcc085f740ab329fe82fc12401ce8a00976dc7
Jackson Ming Hu <huming2207@gmail.com>
2018-04-05 20:35:06 +1000

    ButtonEvent: no need to use JavaFX boxing stuff any more
    Signed-off-by: Jackson Ming Hu <huming2207@gmail.com>

--- src/models/gui/BoardButtonEvent.java
+++ src/models/gui/BoardButtonEvent.java
@@ -7,68 +7,44 @@ import javafx.beans.property.StringProperty;
 
 public class BoardButtonEvent
 {
-    private IntegerProperty posX = new SimpleIntegerProperty();
-    private IntegerProperty posY = new SimpleIntegerProperty();
-    private StringProperty  id = new SimpleStringProperty();
+    private int posX;
+    private int posY;
+    private String id;
 
-    public BoardButtonEvent(IntegerProperty posX, IntegerProperty posY, StringProperty id)
+    public BoardButtonEvent(int posX, int posY, String id)
     {
         this.posX = posX;
         this.posY = posY;
         this.id = id;
     }
 
-    public BoardButtonEvent(int posX, int posY, String id)
-    {
-        this.posX.set(posX);
-        this.posY.set(posY);
-        this.id.set(id);
-    }
-
-
-    public String getId()
-    {
-        return id.get();
-    }
-
-    public StringProperty idProperty()
-    {
-        return id;
-    }
-
-    public void setId(String id)
-    {
-        this.id.set(id);
-    }
 
     public int getPosX()
-    {
-        return posX.get();
-    }
-
-    public IntegerProperty posXProperty()
     {
         return posX;
     }
 
     public void setPosX(int posX)
     {
-        this.posX.set(posX);
+        this.posX = posX;
     }
 
-    public int getPosY()
-    {
-        return posY.get();
-    }
-
-    public IntegerProperty posYProperty()
-    {
+    public int getPosY() {
         return posY;
     }
 
     public void setPosY(int posY)
     {
-        this.posY.set(posY);
+        this.posY = posY;
     }
 
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
 }
