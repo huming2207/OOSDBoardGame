4d4d5e614bcefffbc7ac4157f2751c8e2fecaeb0
Jackson Ming Hu <huming2207@gmail.com>
2018-05-20 18:06:38 +1000

    Bugfixes: log warning reaction

--- src/helpers/reactions/WarningReaction.java
+++ src/helpers/reactions/WarningReaction.java
@@ -2,6 +2,9 @@ package helpers.reactions;
 
 import javafx.scene.control.Alert;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Warning reactions: shows a warning dialog box with logged info
  *
@@ -18,6 +21,9 @@ public class WarningReaction extends AbstractReaction
     @Override
     protected void performReaction(String message)
     {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+        System.out.println(String.format("[WARNING][%s]: %s", dateFormatter.format(new Date()), message));
+
         Alert alert = new Alert(Alert.AlertType.WARNING, message);
         alert.show();
     }
