82fa231d25eb5efe022e9218beb5933af233583d
Jackson Ming Hu <huming2207@gmail.com>
2018-05-21 23:26:34 +1000

    CharacterType: use C/C# style int
    Signed-off-by: Jackson Ming Hu <huming2207@gmail.com>

--- src/models/piece/type/CharacterType.java
+++ src/models/piece/type/CharacterType.java
@@ -2,10 +2,22 @@ package models.piece.type;
 
 public enum CharacterType
 {
-    WECHAT,
-    FACEBOOK,
-    GOOGLE,
-    WEIBO,
-    TWITTER,
-    BAIDU
+    WECHAT(0),
+    FACEBOOK(1),
+    GOOGLE(2),
+    WEIBO(3),
+    TWITTER(4),
+    BAIDU(5);
+
+    private int characterType;
+
+    CharacterType(int characterType)
+    {
+        this.characterType = characterType;
+    }
+
+    public int getCharacterTypeId()
+    {
+        return this.characterType;
+    }
 }
